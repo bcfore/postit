15388d0554e05b3a4da7381f6dee94b0f97285b2
bcfore <b.c.fore@gmail.com>
2017-06-21 19:14:07 -0700

    Up to Lesson 2p9

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615201211) do
+ActiveRecord::Schema.define(version: 20170619192737) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20170615201211) do
     t.text    "body"
     t.integer "user_id"
     t.integer "post_id"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
