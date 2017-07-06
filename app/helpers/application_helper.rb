fb5d12a897368a3ef2cde356ba7cf3c6eb948b4c
bcfore <b.c.fore@gmail.com>
2017-07-06 16:03:42 -0700

    Lesson 3p1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def fix_url(str)
+    str.start_with?('http://') ? str : "http://#{str}"
+  end
+
+  def print_datetime(datetime)
+    if datetime.nil?
+      return
+    else
+      datetime.strftime('%m/%d/%Y %l:%M%P %Z').squeeze(' ')
+    end
+  end
 end
